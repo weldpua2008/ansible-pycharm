e453c6cb2ab5ae03981a54ba203784a05e4fd475
Valeriy Solovyov <weldpua2008@gmail.com>
2015-08-25 13:37:39 +0300

    removed sudo for centos

--- tests/test-on-rpm.sh
+++ tests/test-on-rpm.sh
@@ -28,10 +28,10 @@ cd $RDIR/..
 
 printf "[defaults]\nroles_path = ../" > ansible.cfg
 ansible-playbook -i tests/test-inventory tests/test.yml --syntax-check
-ANSIBLE_SHORT_VERSION=`ansible-playbook --version 2> /dev/null|cut -d " " -f2|cut -d "." -f1,2` ansible-playbook -i tests/test-inventory tests/test.yml --connection=local --sudo
+ANSIBLE_SHORT_VERSION=`ansible-playbook --version 2> /dev/null|cut -d " " -f2|cut -d "." -f1,2` ansible-playbook -i tests/test-inventory tests/test.yml --connection=local 
 
 # Run the role/playbook again, checking to make sure it's idempotent.
-ANSIBLE_SHORT_VERSION=`ansible-playbook --version 2> /dev/null|cut -d " " -f2|cut -d "." -f1,2` ansible-playbook -i tests/test-inventory tests/test.yml --connection=local --sudo | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+ANSIBLE_SHORT_VERSION=`ansible-playbook --version 2> /dev/null|cut -d " " -f2|cut -d "." -f1,2` ansible-playbook -i tests/test-inventory tests/test.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 
 
 exit 0
