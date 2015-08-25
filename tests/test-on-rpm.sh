9e250b5d8cbc5db0744ada2337efee540b5ff523
Valeriy Solovyov <weldpua2008@gmail.com>
2015-08-25 13:24:09 +0300

    try to fix centos issues

--- tests/test-on-rpm.sh
+++ tests/test-on-rpm.sh
@@ -10,7 +10,7 @@ SOURCE="${BASH_SOURCE[0]}"
 RDIR="$( dirname "$SOURCE" )"
 ANSIBLE_VERSION=${1:-latest}
 
-yum -y install epel-release&& yum -y update && yum install gcc glibc glibc-common  -y && yum -y groupinstall  "Development Tools" &&  yum install -y PyYAML libyaml python-babel python-crypto python-simplejson python-paramiko python-pip python-crypto2.6 python-httplib2  python-jinja2  python-keyczar python-pyasn1&& pip install --upgrade pip
+yum -y install epel-release&& yum -y update && yum install gcc glibc glibc-common  -y && yum -y groupinstall  "Development Tools" &&  yum install -y PyYAML libyaml python-babel python-crypto python-simplejson python-paramiko python-pip python-crypto python-httplib2  python-jinja2  python-keyczar python-pyasn1 python-devel&& pip install --upgrade pip
 
 if [ "$ANSIBLE_VERSION" = "latest" ]; then
     pip install --upgrade ansible;
